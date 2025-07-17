896924c019cfa4ceaf7b626be616485998eac6d3
Adhizah <mhalikadhizah123@gmail.com>
2025-07-17 22:29:30 +0000

    Made some minor fixex

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -4,7 +4,7 @@ import { Redirect } from "expo-router";
 import "@/global.css";
 
 const index = () => {
-  return <Redirect href="/(checkout)/CartScreen" />;
+  return <Redirect href="/(orders)/TransactionPage" />;
 };
 
 export default index;
